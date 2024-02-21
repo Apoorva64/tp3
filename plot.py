41710533aaca3dbd276a988cd91ebaaabb147d03
Apoorva Appadoo Srinivas <apoorvaappadoo@gmail.com>
2024-02-21 11:28:34 +0100

    swewfc

--- plot.py
+++ plot.py
@@ -5,37 +5,27 @@ import pickle
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 # load data
-data = base64.b64decode("""gASVqgUAAAAAAABdlCh9lChLCksBhpRHPuVJPWCznv5LCksChpRHPwfrA0OsUi5LCksEhpRHPyHIOqgCzyNLCksIhpRHPy2gcUaLogJLCksQhpRHP0AsoBN9raJLCksghpRHP3DcAyCb1uFLZEsBhpRHPuUeSkLvPeZLZEsChpRHPwgfW4/zqIRLZEsEhpRHPxsBFd0/lEhLZEsIhpRHPy2lLHTTELRLZEsQhpRHP0XprACkaq5LZEsghpRHP3tmmz+MBwpN6ANLAYaURz72/OkWptfoTegDSwKGlEc/EQ6jumJvlE3oA0sEhpRHPyR+1uRslW5N6ANLCIaURz8wONBesEIwTegDSxCGlEc/PCxGBBtCp03oA0sghpRHP4N76IO3hRJNECdLAYaURz8r30hpPyKZTRAnSwKGlEc/LAGTUwOIIE0QJ0sEhpRHPymIgQdAVPRNECdLCIaURz8z28PPZvTFTRAnSxCGlEc/PtMUZJD5N00QJ0sghpRHP4r0kuRzsn9KoIYBAEsBhpRHP2WslPPteBNKoIYBAEsChpRHP1gSu5lzrvlKoIYBAEsEhpRHP1Ka14c68kRKoIYBAEsIhpRHP0nWQg3j5bpKoIYBAEsQhpRHP0+t9IlDJAZKoIYBAEsghpRHP40Z+aZJq0VKQEIPAEsBhpRHP58iHw65AGdKQEIPAEsChpRHP4945BdsLVBKQEIPAEsEhpRHP4ObQrwSP3NKQEIPAEsIhpRHP3jov0anWUtKQEIPAEsQhpRHP3UhLHV+3StKQEIPAEsghpRHP5dEGF3E1zVKgJaYAEsBhpRHP9k8bJfYzzpKgJaYAEsChpRHP8pbyH2ys0ZKgJaYAEsEhpRHP79j5KvmozhKgJaYAEsIhpRHP8IUeuFHrhRKgJaYAEsQhpRHP8FmL9/BnBdKgJaYAEsghpRHP8Hy5I6Kcd51fZQoSwpLAYaURz7C/ecpou82SwpLAoaURz67kKGavFDLSwpLBIaURz65YkkXw2KNSwpLCIaURz7GWws+d4n6SwpLEIaURz66Y/vKXakgSwpLIIaURz69bV9lJoaOS2RL
-AYaURz7F76t0DJc9S2RLAoaURz7FWViL3UNoS2RLBIaURz7GyJDKX4GSS2RLCIaURz7GRZGvlVluS2RLEIaURz7Dq9liMXhzS2RLIIaURz7FLmVuGOJPTegDSwGGlEc+8DiyThtr7E3oA0sChpRHPvgUnsiCkD5N6ANLBIaURz7vtXz5+68N
-TegDSwiGlEc+70odL5C8UE3oA0sQhpRHPvcCR3pfZAtN6ANLIIaURz7wO6Y4J1KZTRAnSwGGlEc/KVO5D6uZoU0QJ0sChpRHPymEx4urgIhNECdLBIaURz8pR/H+gfMBTRAnSwiGlEc/LlWrgyYPtE0QJ0sQhpRHPyl0AJQKyppNECdLIIaU
-Rz8p+SVwUfeZSqCGAQBLAYaURz9kHe8pQxpcSqCGAQBLAoaURz9kU4mU6bGKSqCGAQBLBIaURz9jjjzEK5OCSqCGAQBLCIaURz9jmEMCXikrSqCGAQBLEIaURz9j+OYFWAOiSqCGAQBLIIaURz9kCTbgZt2FSkBCDwBLAYaURz+dgsEQAChE
-SkBCDwBLAoaURz+ctcikSSAFSkBCDwBLBIaURz+h2Pkq+ajOSkBCDwBLCIaURz+gn82QQStDSkBCDwBLEIaURz+e6XIfAU0HSkBCDwBLIIaURz+eWwXgE9FuSoCWmABLAYaURz/Wxz7uUliSSoCWmABLAoaURz/X38GcFyJbSoCWmABLBIaURz/WikO7QLNOSoCWmABLCIaURz/Xw6hg3LmqSoCWmABLEIaURz/X2FWXC0ngSoCWmABLIIaURz/V4Ajps41hdV2UKEsKS2RN6ANNECdKoIYBAEpAQg8ASoCWmABlXZQoSwFLAksESwhLEEsgZWUu
+data = base64.b64decode("""gASV0wEAAAAAAABdlCh9lChNECdLAYaURz843Qp2allUTRAnSwKGlEc/OgCHOW+4Sk0QJ0sEhpRHPziHqV+93lBNECdLCIaURz+CbZ8/y8fqTRAnSxCGlEc/jnwvJYd5Bk0QJ0sghpRHP5RydxXhEtVKoIYBAEsBhpRHP3HbmkUQKnBKoIYB
+AEsChpRHP2TYVD9yW71KoIYBAEsEhpRHP2F/U/McQdVKoIYBAEsIhpRHP3Wt2cJ+lTFKoIYBAEsQhpRHP472gCR9jcpKoIYBAEsghpRHP5lAUFIn6wFKQEIPAEsBhpRHP6pZxxuy4+1KQEIPAEsChpRHP5wxAgCLQDtKQEIPAEsEhpRHP5hZ
+/nkXQxNKQEIPAEsIhpRHP5As7nWUANNKQEIPAEsQhpRHP5Ufeu+KZvdKQEIPAEsghpRHP6ESnz71CFVKgJaYAEsBhpRHP+R889wFTvRKgJaYAEsChpRHP9TnuAqd6LRKgJaYAEsEhpRHP8i2hIvrWy1KgJaYAEsIhpRHP8OiL2pQ1rJKgJaYAEsQhpRHP8OUbDMvAXVKgJaYAEsghpRHP8M2BJ7LMcJ1XZQoTRAnSqCGAQBKQEIPAEqAlpgAZV2UKEsBSwJLBEsISxBLIGVlLg==
 """)
 
-openmp_results, no_openmp_results, SIZE_ARRAY, THREADS_ARRAY = pickle.loads(data)
+openmp_results, SIZE_ARRAY, THREADS_ARRAY = pickle.loads(data)
 
 
-# make a plot by varying the number of threads and the size of the array
+# make a plot by varying the number of threads and the size of the array 2d
 
 fig = plt.figure()
-ax1 = fig.add_subplot(121, projection='3d')
-ax2 = fig.add_subplot(122, projection='3d')
+ax1 = fig.add_subplot(111, projection='3d')
 
 # Extract data for openmp and no-openmp
 x_openmp, y_openmp = zip(*openmp_results.keys())
 z_openmp = list(openmp_results.values())
 
-x_no_openmp, y_no_openmp = zip(*no_openmp_results.keys())
-z_no_openmp = list(no_openmp_results.values())
-
 # Reshape data for 3D plotting
 x_openmp = np.array(x_openmp).reshape(len(SIZE_ARRAY), len(THREADS_ARRAY))
 y_openmp = np.array(y_openmp).reshape(len(SIZE_ARRAY), len(THREADS_ARRAY))
 z_openmp = np.array(z_openmp).reshape(len(SIZE_ARRAY), len(THREADS_ARRAY))
-
-x_no_openmp = np.array(x_no_openmp).reshape(len(SIZE_ARRAY), len(THREADS_ARRAY))
-y_no_openmp = np.array(y_no_openmp).reshape(len(SIZE_ARRAY), len(THREADS_ARRAY))
-z_no_openmp = np.array(z_no_openmp).reshape(len(SIZE_ARRAY), len(THREADS_ARRAY))
 
 # Plotting the openmp results
 ax1.set_title('OpenMP')
@@ -44,12 +34,4 @@ ax1.set_ylabel('Threads')
 ax1.set_zlabel('Time (s)')
 # set size in power of 10
 ax1.plot_surface(x_openmp, y_openmp, z_openmp, cmap='viridis', edgecolor='k')
-
-# Plotting the no-openmp results
-ax2.set_title('No OpenMP')
-ax2.set_xlabel('Size')
-ax2.set_ylabel('Threads')
-ax2.set_zlabel('Time (s)')
-ax2.plot_surface(x_no_openmp, y_no_openmp, z_no_openmp, cmap='viridis', edgecolor='k')
-
 plt.show()
--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 # make a plot by varying the number of threads and the size of the array
-SIZE_ARRAY = [ 10000, 100000, 1000000, 10000000, 100000000, 1000000000]
+SIZE_ARRAY = [ 10000, 100000, 1000000, 10000000]
 THREADS_ARRAY = [1, 2, 4, 8, 16, 32]
 
 EXEC_PATH_OPENMP= "cmake-build-debug/tp3_openmp"
@@ -11,7 +11,8 @@ import subprocess
 from pathlib import Path
 
 
-def run_solver(exec_path: str, size: int, threads:int ) -> str:
+def run_solver(exec_path: str, size: int, threads: int ) -> float:
+    print(exec_path, size, threads)
     result = subprocess.run([exec_path, str(size), str(threads)], capture_output=True)
     return float(result.stdout.decode("utf-8"))
 
@@ -19,13 +20,11 @@ def run_solver(exec_path: str, size: int, threads:int ) -> str:
 
 # openmp
 openmp_results = {}
-no_openmp_results = {}
 for size in SIZE_ARRAY:
     for threads in THREADS_ARRAY:
         openmp_results[(size, threads)] = run_solver(EXEC_PATH_OPENMP, size, threads)
-        no_openmp_results[(size, threads)] = run_solver(EXEC_PATH_NO_OPENMP, size, threads)
 
-data = [openmp_results, no_openmp_results, SIZE_ARRAY, THREADS_ARRAY]
+data = [openmp_results, SIZE_ARRAY, THREADS_ARRAY]
 
 # store in base64
 import base64
